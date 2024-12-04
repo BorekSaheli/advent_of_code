f2558ac93d0fd8d4267abce498f4859e75707847
Borek <boreksaheli@gmail.com>
2024-12-04 19:07:26 +0100

    day 4 clean up code

--- 2024/day_04/part2.py
+++ 2024/day_04/part2.py
@@ -1,3 +1,7 @@
+import time
+
+start_time = time.time()
+
 with open("input.txt", "r") as file:
     data = file.readlines()
 
@@ -8,34 +12,27 @@ A_pos = []
 xmas_countr = 0
 
 for row in range(len(data) - 0):
-    # print(data[row])
     for col in range(len(data[row])):
-        # print(data[row][col])
-        # positions
         to_top_edge = row + 1
-        to_bot_edge = len(data) - row - 0
+        to_bot_edge = len(data) - row
         to_left_edge = col + 1
-        to_right_edge = len(data[row]) - col - 0
-        print(f"current_char:={data[row][col]}=")
-        print(
-            f"top:{to_top_edge},bot:{to_bot_edge}, left:{to_left_edge}, right:{to_right_edge}"
-        )
+        to_right_edge = len(data[row]) - col
+
+        # print(f"current_char:={data[row][col]}=")
+        # print(
+        #     f"top:{to_top_edge},bot:{to_bot_edge}, left:{to_left_edge}, right:{to_right_edge}"
+        # )
 
         if to_bot_edge >= len(word) and to_right_edge >= len(word):  # SE
             counter = 0
             for i in range(len(word)):
-                # print(i, "-", data[row + i][col + i])
                 if word[counter] == data[row + i][col + i]:
                     counter += 1
-                    print(f"Matched {word[counter-1]} with {data[row + i][col + i]}")
-                    if counter == len(word) - 0:
-
-
+                    # print(f"Matched {word[counter-1]} with {data[row + i][col + i]}")
+                    if counter == len(word):
                         xmas_countr += 1
-                        print(f"found Xmas {xmas_countr}")
-                        print(data[row +1][col +1],[row +1 , col +1], "SE"  )
-                        A_pos.append([row +1, col +1])
-                        print(A_pos)
+                        print(data[row + 1][col + 1], [row + 1, col + 1], "SE")
+                        A_pos.append([row + 1, col + 1])
                         break
                 else:
                     break
@@ -43,19 +40,13 @@ for row in range(len(data) - 0):
         if to_bot_edge >= len(word) and to_left_edge >= len(word):  # SW
             counter = 0
             for i in range(len(word)):
-                # print(i, "-", data[row + i][col - i])
                 if word[counter] == data[row + i][col - i]:
                     counter += 1
-                    print(f"Matched {word[counter-1]} with {data[row + i][col - i]}")
+                    # print(f"Matched {word[counter-1]} with {data[row + i][col - i]}")
                     if counter == len(word) - 0:
-
-
                         xmas_countr += 1
-                        print(f"found Xmas {xmas_countr}")
-                        A_pos.append([row + 1, col - 1 ])
-                        print(data[row +1][col -1], [row +1] , [col -1],"SW")
-                        
-                        print(A_pos)
+                        A_pos.append([row + 1, col - 1])
+                        print(data[row + 1][col - 1], [row + 1, col - 1], "SW")
                         break
                 else:
                     break
@@ -66,17 +57,11 @@ for row in range(len(data) - 0):
                 # print(i, "-", data[row - i][col + i])
                 if word[counter] == data[row - i][col + i]:
                     counter += 1
-                    print(f"Matched {word[counter-1]} with {data[row - i][col + i]}")
+                    # print(f"Matched {word[counter-1]} with {data[row - i][col + i]}")
                     if counter == len(word) - 0:
-
-
                         xmas_countr += 1
-                        print(f"found Xmas {xmas_countr}")
-                        A_pos.append([row - 1 , col + 1])
-                        print(data[row -1][col +1], [row -1] , [col +1],"NE")
-
-                        print(A_pos)
-
+                        A_pos.append([row - 1, col + 1])
+                        print(data[row - 1][col + 1], [row - 1, col + 1], "NE")
                         break
                 else:
                     break
@@ -84,37 +69,23 @@ for row in range(len(data) - 0):
         if to_top_edge >= len(word) and to_left_edge >= len(word):  # NW
             counter = 0
             for i in range(len(word)):
-                # print(i, "-", data[row - i][col - i])
                 if word[counter] == data[row - i][col - i]:
                     counter += 1
-                    print(f"Matched {word[counter-1]} with {data[row - i][col - i]}")
+                    # print(f"Matched {word[counter-1]} with {data[row - i][col - i]}")
                     if counter == len(word) - 0:
-
                         xmas_countr += 1
-                        print(f"found Xmas {xmas_countr}")
                         A_pos.append([row - 1, col - 1])
-                        print(data[row -1][col -1], [row -1] , [col -1],"NW")
-
-                        print(A_pos)
-
+                        print(data[row - 1][col - 1], [row - 1, col - 1], "NW")
                         break
                 else:
                     break
 
 
-
-print(xmas_countr)
-print(A_pos)
 literal_x_counter = 0
-
 for i in range(len(A_pos)):
     for j in range(i + 1, len(A_pos)):
         if A_pos[i] == A_pos[j]:
-            print(A_pos[i], A_pos[j])
             literal_x_counter += 1
 
 print("Total duplicate pairs:", literal_x_counter)
-
-
-
-
+print("--- %s seconds ---" % (time.time() - start_time))
