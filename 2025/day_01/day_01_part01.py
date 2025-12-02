1a2ea006b292a3e52dca968faba27495be66b594
borek.saheli <boreksaheli@gmail.com>
2025-12-02 15:31:40 +0100

    removed: unneeded list

--- 2025/day_01/day_01_part01.py
+++ 2025/day_01/day_01_part01.py
@@ -25,13 +25,12 @@ def parse_direction(code: str) -> int:
 
 
 def main(input: list[str], start_val: int = 50) -> int:
-    spin_values = list(range(0, 100))
     current_position = start_val
     counter = 0
 
     for code in input:
         spin_val = (parse_direction(code) + current_position) % 100
-        current_position = spin_values[spin_val]
+        current_position = spin_val
 
         if current_position == 0:
             counter += 1
