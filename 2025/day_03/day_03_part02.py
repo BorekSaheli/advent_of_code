d3f45260f4ff79d786902a68a84d7bd1bd287a63
borek.saheli <boreksaheli@gmail.com>
2025-12-10 14:28:12 +0100

    refactor: day 3 part 2

--- 2025/day_03/day_03_part02.py
+++ 2025/day_03/day_03_part02.py
@@ -12,48 +12,26 @@ data = data.split("\n")
 def find_joltage(
     to_flip: int,
     line: str,
-)-> int:
-
-    # init spaces
+) -> int:
     start_search_pos = 0
     end_search_pos = len(line) - to_flip + 1
-    exclusion_space = line[:start_search_pos]
     search_space = line[start_search_pos:end_search_pos]
-    buffer_space = line[end_search_pos:]
 
     current_joltage = ""
 
-    for i in range(len(line)):
+    while start_search_pos + 1 < end_search_pos and search_space:
         max_num = max(search_space)
-        for idx, num in enumerate(search_space):
-            if max_num == num:
-                max_num_pos = idx
-                break
+        max_num_pos = search_space.find(max_num)
 
-        # update spaces 
-        start_search_pos = max_num_pos + 1 + len(exclusion_space)
+        start_search_pos = max_num_pos + 1 + start_search_pos
         end_search_pos += 1
+        search_space = line[start_search_pos:end_search_pos]
+
         current_joltage = current_joltage + max_num
 
-        exclusion_space = line[:start_search_pos]
-        search_space = line[start_search_pos:end_search_pos]
-        buffer_space = line[end_search_pos - 1 :]
+    current_joltage += line[end_search_pos - 1 :]
 
-        # condtion to break
-        # run out of search space
-        if search_space == "":
-            break
-
-        # next max number at the boundry of the buffer start
-        if start_search_pos + 1 == end_search_pos:
-            current_joltage = current_joltage + buffer_space
-            break
-
-    # trim output when max number at boundty of buffer if buffer and current joltage exeed to_flip
-    if len(current_joltage) > to_flip:
-        current_joltage = current_joltage[:to_flip]
-
-    return int(current_joltage)
+    return int(current_joltage[:to_flip])
 
 
 total_joltage = 0
